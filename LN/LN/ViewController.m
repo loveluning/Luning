56e0090b5a2f604dbaa7f8dbd460f213f820a12c
陆宁 <luning@luningtekimacbook-air.local>
2015-10-26 16:54:08 +0800

    aa

---
+++ LN/LN/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LN
+//
+//  Created by 陆宁 on 15/10/26.
+//  Copyright © 2015年 LN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LN/LN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LN
+//
+//  Created by 陆宁 on 15/10/26.
+//  Copyright © 2015年 LN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
